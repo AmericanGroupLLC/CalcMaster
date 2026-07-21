fd6ac851ad5f9586a760ba4c54e9420d4847cfe1
AGL Dev <dev@americangroupllc.com>
2026-07-21 13:12:23 -0700

    iOS: build without storyboards for simulator install (headless ibtool workaround)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,16 @@
 import Flutter
 import UIKit
-
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+@objc class AppDelegate: FlutterAppDelegate {
+  private let flutterEngine = FlutterEngine(name: "app_engine")
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    flutterEngine.run()
+    GeneratedPluginRegistrant.register(with: flutterEngine)
+    let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    window.rootViewController = flutterViewController
+    window.makeKeyAndVisible()
+    self.window = window
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
